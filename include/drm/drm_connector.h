2a6568c77973b56e2c078bb4adb1765cce51f5c9
Balázs Triszka <info@balika011.hu>
2026-07-08 23:24:39 +0200

    Revert "drm/panel: Add a panel notifier list into drm_panel"
    This reverts commit a14a49cb734153365835a6e3c6a822dfa592c93a.

--- include/drm/drm_connector.h
+++ include/drm/drm_connector.h
@@ -39,7 +39,6 @@ struct drm_encoder;
 struct drm_property;
 struct drm_property_blob;
 struct drm_printer;
-struct drm_panel;
 struct edid;
 
 enum drm_connector_force {
@@ -1208,13 +1207,6 @@ struct drm_connector {
 	 * &drm_mode_config.connector_free_work.
 	 */
 	struct llist_node free_node;
-
-	/**
-	 * @panel:
-	 *
-	 * Can find the panel which connected to drm_connector.
-	 */
-	struct drm_panel *panel;
 
 	/**
 	 * @checksum:
